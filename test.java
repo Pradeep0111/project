aa6aa7278fbc93a4d7d5d41c10f2cf3cd8db3ac1
Pradeep0111 <84283114+Pradeep0111@users.noreply.github.com>
2021-08-11 12:25:30 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,16 @@
+public class test
+{  
+public static void main(String args[])  
+{    
+ int a=0,b=1,c;    
+ System.out.print(a+" "+b);   
+    
+ for(int i=2;i<=10;++i)    
+ {    
+  c=a+b;    
+  System.out.print(" "+c);    
+  a=b;    
+  b=c;    
+ }    
+  
+}}  
